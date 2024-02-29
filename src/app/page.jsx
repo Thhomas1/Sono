626e8218edeb58fa2a0e051701a6777edde41bcd
Thhomas1 <thomasromero10@hotmail.com>
2024-02-29 10:00:02 -0300

    home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,26 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import construction from "../../public/logonegro.png";
+import imagehome from "../../public/imagehomepage.png";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>
+          Los mejores haciendo maquinas para sus clientes.
+        </h1>
+        <p className={styles.desc}>
+          Convertimos tus suenitos en realidad gracias a nuestras maquinas.
+        </p>
+        <button className={styles.button} href="/contact">
+          Contactanos
+        </button>
+      </div>
+      <div>
+        <Image className={styles.img} src={imagehome} alt=""></Image>
+      </div>
+    </div>
+  );
 }
-// 45:25
+// 55:00
